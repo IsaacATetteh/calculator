7094b086f2ea3cb444096b690b5bdbd6e14d15e5
IsaacATetteh <iabbeytetteh@gmail.com>
2023-11-09 17:42:49 +0000

    Button templates

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,20 @@
 import Image from "next/image";
 
 export default function Home() {
+  const whiteButton =
+    "flex items-center justify-center w-[83px] h-[50px] bg-[#E9E3DA] rounded-lg shadow-custom";
+  const blueButton =
+    "flex items-center justify-center w-[83px] h-[50px] bg-[#A2B2E1] rounded-lg shadow-customblue";
   return (
-    <div className="w-full h-full flex flex-col justify-center items-center bg-[#3B4664] gap-4">
-      <div className="w-[490px] h-[115px] bg-[#181F32] rounded-md"></div>
-      <div className="w-[490px] h-[50%] rounded-md bg-[#262C45] grid grid-cols-5 grid-rows-4">
-        <div className="w-[95px] h-[60px] border-2 border-black"></div>
-        <div className="w-[95px] h-[60px] border-2 border-black"></div>
-        <div className="w-[95px] h-[60px] border-2 border-black"></div>
-        <div className="w-[95px] h-[60px] border-2 border-black"></div>
+    <div className="w-full h-full flex flex-col justify-center items-center bg-[#3B4664] font-league-spartan gap-4">
+      <div className="w-[440px] h-[100px] bg-[#181F32] rounded-md"></div>
+      <div className="w-[440px] h-[435px] rounded-md bg-[#262C45] flex justify-center items-center">
+        <div className="w-[390px] border-2 border-white h-[380px] rounded-md bg-[#262C45] grid grid-cols-4 grid-rows-5 gap-4">
+          <button className={whiteButton}>9</button>
+          <button className={whiteButton}>8</button>
+          <button className={whiteButton}>7</button>
+          <button className={blueButton}>7</button>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      boxShadow: {
+        custom: "0 4px 1px 0 #B3A195",
+        customblue: "0 4px 1px 0 #414E73",
       },
     },
   },
   plugins: [],
-}
+};
